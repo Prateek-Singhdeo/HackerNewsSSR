71198e9b0a50a90bad72f1b3d687badac99e2b6a
Singhdeo <prateek.singhdeo@sap.com>
2020-07-19 16:20:13 +0530

    component changes

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ import * as actionTypes from './store/actions/actionTypes';
 const app = express();
 app.use(cookieParser())
 app.use( express.static( path.resolve( __dirname, "../dist" ) ) );
-app.use("/public" ,express.static(path.resolve(__dirname, "../public")));
+// app.use("/public" ,express.static(path.resolve(__dirname, "../public")));
 const port = process.env.PORT || 3000;
 
 app.get( "/*", ( req, res ) => {
@@ -87,7 +87,6 @@ function htmlTemplate( reactDom, reduxState, helmet) {
         <html lang="en">
         <head>
             ${helmet.meta.toString()}
-            <link rel="icon" href="/public/favicon.ico" />
             <link rel="stylesheet" type="text/css" href="./styles.css" />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         </head>
